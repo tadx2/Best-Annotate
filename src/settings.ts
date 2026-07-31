f25a1afad0c8d9db826c4ce1ff66d13299010ada
tadx2 <tadx1024@gmail.com>
2026-07-31 09:49:56 +0800

    first commit: following the official tutorial - Getting Started: Build a plugin

--- src/settings.ts
+++ src/settings.ts
@@ -1,5 +1,5 @@
 import { App, PluginSettingTab, Setting } from 'obsidian';
-import MyPlugin from './main';
+import HelloWorldPlugin from './main';
 
 export interface MyPluginSettings {
 	mySetting: string;
@@ -10,9 +10,9 @@ export const DEFAULT_SETTINGS: MyPluginSettings = {
 };
 
 export class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: HelloWorldPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: HelloWorldPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
